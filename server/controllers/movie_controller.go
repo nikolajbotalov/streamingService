d0a4645b7061c62e90b660dff3f61fa9b5d6a974
reactguy <nikolaj.botalov@gmail.com>
2025-10-28 21:20:15 +0500

    feat: implement AdminReviewUpdate method

--- server/controllers/movie_controller.go
+++ server/controllers/movie_controller.go
@@ -2,18 +2,26 @@ package controllers
 
 import (
 	"context"
+	"errors"
 	"github.com/gin-gonic/gin"
 	"github.com/go-playground/validator/v10"
+	"github.com/joho/godotenv"
+	"github.com/tmc/langchaingo/llms/openai"
 	"go.mongodb.org/mongo-driver/v2/bson"
 	"go.mongodb.org/mongo-driver/v2/mongo"
+	"log"
 	"net/http"
+	"os"
 	"server/database"
 	"server/models"
+	"strings"
 	"time"
 )
 
 var movieCollection *mongo.Collection = database.OpenCollection("movies")
 var movieValidate = validator.New()
+
+var rankingCollection *mongo.Collection = database.OpenCollection("rankings")
 
 func GetMovies() gin.HandlerFunc {
 	return func(c *gin.Context) {
@@ -83,4 +91,135 @@ func AddMovie() gin.HandlerFunc {
 
 		c.JSON(http.StatusCreated, result)
 	}
+}
+
+func AdminReviewUpdate() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		movieID := c.Param("imdb_id")
+		if movieID == "" {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Movie ID required"})
+			return
+		}
+
+		var req struct {
+			AdminReview string `json:"admin_review"`
+		}
+		var resp struct {
+			RankingName string `json:"ranking_name"`
+			AdminReview string `json:"admin_review"`
+		}
+
+		if err := c.ShouldBindJSON(&req); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request body"})
+			return
+		}
+
+		sentiment, rankValue, err := GetReviewRanking(req.AdminReview)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error getting review ranking"})
+			return
+		}
+
+		filter := bson.M{"imdb_id": movieID}
+
+		update := bson.M{
+			"$set": bson.M{
+				"admin_review": req.AdminReview,
+				"ranking": bson.M{
+					"ranking_value": rankValue,
+					"ranking_name":  sentiment,
+				},
+			},
+		}
+
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+		defer cancel()
+
+		result, err := movieCollection.UpdateOne(ctx, filter, update)
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": "Error updating movie"})
+			return
+		}
+		if result.MatchedCount == 0 {
+			c.JSON(http.StatusNotFound, gin.H{"error": "Movie not found"})
+			return
+		}
+
+		resp.RankingName = sentiment
+		resp.AdminReview = req.AdminReview
+
+		c.JSON(http.StatusOK, resp)
+	}
+}
+
+func GetReviewRanking(adminReview string) (string, int, error) {
+	rankings, err := GetRankings()
+	if err != nil {
+		return "", 0, err
+	}
+
+	sentimentDelimited := ""
+
+	for _, ranking := range rankings {
+		if ranking.RankingValue != 999 {
+			sentimentDelimited = sentimentDelimited + ranking.RankingName + ","
+		}
+	}
+
+	sentimentDelimited = strings.Trim(sentimentDelimited, ",")
+
+	err = godotenv.Load(".env")
+	if err != nil {
+		log.Println("Warning: .env file not found")
+		return "", 0, err
+	}
+
+	openAiApiKey := os.Getenv("OPENAI_API_KEY")
+	if openAiApiKey == "" {
+		return "", 0, errors.New("could not read OPENAI_API_KEY")
+	}
+
+	llm, err := openai.New(openai.WithToken(openAiApiKey))
+	if err != nil {
+		return "", 0, err
+	}
+
+	basePromptTemplate := os.Getenv("BASE_PROMPT_TEMPLATE")
+
+	basePrompt := strings.Replace(basePromptTemplate, "{rankings}", sentimentDelimited, 1)
+
+	response, err := llm.Call(context.Background(), basePrompt+adminReview)
+	if err != nil {
+		return "", 0, err
+	}
+
+	rankVal := 0
+
+	for _, ranking := range rankings {
+		if ranking.RankingName == response {
+			rankVal = ranking.RankingValue
+			break
+		}
+	}
+
+	return response, rankVal, nil
+}
+
+func GetRankings() ([]models.Ranking, error) {
+	var rankings []models.Ranking
+
+	var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+	defer cancel()
+
+	cursor, err := rankingCollection.Find(ctx, bson.M{})
+	if err != nil {
+		return nil, err
+	}
+	defer cursor.Close(ctx)
+
+	if err := cursor.All(ctx, &rankings); err != nil {
+		return nil, err
+	}
+
+	return rankings, nil
 }
