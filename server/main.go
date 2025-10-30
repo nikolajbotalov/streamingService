ca48cbafa33f563de5793dbff234ee09e6d4fc91
reactguy <nikolaj.botalov@gmail.com>
2025-10-30 19:52:20 +0500

    feat: checking connect & disconnect ro db

--- server/main.go
+++ server/main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 	"go.mongodb.org/mongo-driver/v2/mongo"
+	"log"
 	"server/database"
 	"server/routes"
 )
@@ -15,12 +18,27 @@ func main() {
 		c.String(200, "Hello world")
 	})
 
+	err := godotenv.Load(".env")
+	if err != nil {
+		log.Println("unable to load .env file")
+	}
+
 	var client *mongo.Client = database.Connect()
+	if err := client.Ping(context.Background(), nil); err != nil {
+		log.Fatalf("Faiiled to reach server: %v", err)
+	}
+
+	defer func() {
+		err := client.Disconnect(context.Background())
+		if err != nil {
+			log.Fatalf("Failed to disconnect from MongoDB: %v", err)
+		}
+	}()
 
 	routes.SetupUnprotectedRoutes(router, client)
 	routes.SetupProtectedRoutes(router, client)
 
-	err := router.Run(":2000")
+	err = router.Run(":2000")
 	if err != nil {
 		fmt.Println("Failed to start server", err)
 		return
